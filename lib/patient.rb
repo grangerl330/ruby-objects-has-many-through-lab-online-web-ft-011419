c7ca5b999a0ed87fcd9bc7c58cef08ad6f29ac3e
Luke Granger <grangerl330@gmail.com>
2019-01-30 17:03:38 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,5 +13,15 @@ class Patient
   end 
   
   def new_appointment(doctor, date)
-    Appointment.new()
+    Appointment.new(date, self, doctor)
+  end 
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end 
+  
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end 
+  
 end 
